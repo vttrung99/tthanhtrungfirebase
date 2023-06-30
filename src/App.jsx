bc56072016c720d07b87041fc93cb87b20e44aad
vttrung99 <92thanhtrung@gmail.com>
2023-06-30 17:09:26 +0700

    lam qua troi cho nay  phai hoc lai roi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import './App.scss'
+import LazyLoad from './LazyLoad';
+import { Routes, Route } from 'react-router-dom';
+import About from './pages/Homes/About/About';
+import Navbar from './components/Loadings/navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      1111111
+      <div className='app-container'>
+        <Navbar></Navbar>
+        <About></About>
+        <Routes>
+        <Route path="" element={LazyLoad(() => import("@pages/Homes/Home"))()} />
+        </Routes>
+      </div>
     </div>
   );
 }
